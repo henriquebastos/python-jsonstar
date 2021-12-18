e2e778fc0dc0b3225046903c83098b751b7fc7ab
Henrique Bastos <henrique@bastos.net>
2021-12-18 12:25:04 -0300

    Simplify BareUrlSession

--- python-eduzz/eduzz/sessions.py
+++ python-eduzz/eduzz/sessions.py
@@ -7,30 +7,14 @@ ENDPOINT = 'https://api2.eduzz.com/'
 
 # Taken from https://github.com/requests/toolbelt/
 class BaseUrlSession(requests.Session):
-    base_url = None
-
-    def __init__(self, base_url=None):
-        if base_url:
-            self.base_url = base_url
+    def __init__(self, base_url):
+        self.base_url = base_url
         super(BaseUrlSession, self).__init__()
 
-    def request(self, method, url, *args, **kwargs):
-        """Send the request after generating the complete URL."""
-        url = self.create_url(url)
-        return super(BaseUrlSession, self).request(
-            method, url, *args, **kwargs
-        )
-
-    def prepare_request(self, request, *args, **kwargs):
+    def prepare_request(self, request):
         """Prepare the request after generating the complete URL."""
-        request.url = self.create_url(request.url)
-        return super(BaseUrlSession, self).prepare_request(
-            request, *args, **kwargs
-        )
-
-    def create_url(self, url):
-        """Create the URL based off this partial path."""
-        return urljoin(self.base_url, url)
+        request.url = urljoin(self.base_url, request.url)
+        return super(BaseUrlSession, self).prepare_request(request)
 
 
 class EduzzAPIError(requests.HTTPError):
